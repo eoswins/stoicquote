4e681e2da31438e90a98933576a310341860d4a1
eoswins <yo0h0o@yahoo.com>
2013-10-18 10:21:36 -0400

    massive indentation errors and added try/block at bottom so files place would not get corrupted due to connectivity errors

--- stoicquote.py
+++ stoicquote.py
@@ -9,10 +9,10 @@ Description: Posts a tweet related to Stoicism
 import tweepy, access, time
 
 class Login:
-'''
-Logs into the user's account using software application's and user's credentials
-'''
-
+    """
+    Logs into the user's account using software application's
+    and user's credentials
+    """
     def __init__(self):
         self.cons_key = access.cons_key
         self.cons_secret = access.cons_secret
@@ -23,19 +23,18 @@ Logs into the user's account using software application's and user's credentials
         self.auth.set_access_token(self.access_token,self.access_token_secret)
 
     def getAPI(self):
-    '''
-    Returns the main api object from the tweety twitter api to interact with
-    twitter
-    '''
+        """
+        Returns the main api object from the tweety twitter api to interact with
+        twitter
+        """
         self.api = tweepy.API(self.auth)
         return self.api
         
 
 class QuoteHandler:
-'''
-Handles pulling the next quote to the Stoicwriter
-
-'''
+    """
+    Handles pulling the next quote to the Stoicwriter
+    """
 
 
     def __init__(self,p):
@@ -47,9 +46,9 @@ Handles pulling the next quote to the Stoicwriter
         self.cut = 0
 
     def yankMessage(self):
-    '''
-    opens the file and returns the next quote
-    '''
+        """
+        opens the file and returns the next quote
+        """
         self.f = open(self.p,'r')
         self.txt = self.f.readlines()
         self.f.close()
@@ -69,19 +68,19 @@ Handles pulling the next quote to the Stoicwriter
         return self.msg
     
     def putLastMessage(self):
-    '''
-    Use in case there is an error uploading the tweet. Reverts the file back to
-    before it was overwritten
-    '''
+        """
+        Use in case there is an error uploading the tweet. Reverts the file back to
+        before it was overwritten
+        """
         self.f = open(self.p,'w')
         self.f.write(self.txt)
         self.f.close()
 
 
 class Stoicwriter:
-'''
-Uses the Login and QuoteHandler class to publish tweets
-'''
+    """
+    Uses the Login and QuoteHandler class to publish tweets
+    """
 
     def __init__(self,login,quotehandler):
         self.login = login
@@ -91,10 +90,10 @@ Uses the Login and QuoteHandler class to publish tweets
         self.message_list = []
 
     def ensureLimit(self,next_message):
-    '''
-    Ensures each twitter message we send is not over 140 characters.
-    If they are, it breaks them down and puts them in message_list
-    '''
+        """
+        Ensures each twitter message we send is not over 140 characters.
+        If they are, it breaks them down and puts them in message_list
+        """
         i=139
         self.next_message = next_message
         if len(self.next_message) > 140:
@@ -106,10 +105,10 @@ Uses the Login and QuoteHandler class to publish tweets
             self.message_list.append(self.next_message)
 
     def tweet(self):
-    '''
-    parses through message_list to send our tweets and sleeps 4 seconds after
-    each attempt    
-    '''
+        """
+        parses through message_list to send our tweets and sleeps 4 seconds after
+        each attempt    
+        """
         self.ensureLimit(self.next_message)
         for message in self.message_list:
             self.api.update_status(message)
@@ -119,6 +118,10 @@ Uses the Login and QuoteHandler class to publish tweets
 login = Login()
 path = 'data/meditations/meditations_pull.txt'
 quotehandler = QuoteHandler(path)
-stoicwriter = Stoicwriter(login, quotehandler)
-stoicwriter.tweet()
+try:
+    stoicwriter = Stoicwriter(login, quotehandler)
+    stoicwriter.tweet()
+except:
+    quotehandler.putLastMessage()
+
     
